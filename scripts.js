19ec4ddc9590b1faf8ad4a3ea7c43491f4443481
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2019-04-26 19:01:27 -0500

    Correccion a inicio de sesion y registro

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+function iniciarSesion(usuario) {
+  localStorage.setItem("usuario", usuario);
+}
+
 function registrarUsuario() {
   if (document.getElementById("user").value.length > 0) {
   const pass = document.getElementById("password1").value;
@@ -9,6 +13,7 @@ function registrarUsuario() {
     };
     if (usuarios === null) {
       localStorage.setItem("usuarios", JSON.stringify({usersArray: [usuarioNuevo]}));
+      iniciarSesion(correo);
       return true;
     } else {
       let arregloUsuarios = JSON.parse(localStorage.getItem("usuarios")).usersArray;
@@ -30,10 +35,17 @@ function buscarUsuario() {
     usuario: document.getElementById("user").value.toLowerCase(),
     clave: document.getElementById("password").value
   }
-  if (JSON.parse(localStorage.getItem("usuarios")).usersArray.some(function (elemento) {
-    return elemento.usuario === intentoLogin.usuario && elemento.clave === intentoLogin.clave;
-  })) {
-    localStorage.setItem("usuario", intentoLogin.usuario);
+  let findUser = false;
+  const itemUsuarios = localStorage.getItem("usuarios");
+  if (itemUsuarios != null) {
+    if (JSON.parse(itemUsuarios).usersArray.some(function (elemento) {
+      return elemento.usuario === intentoLogin.usuario && elemento.clave === intentoLogin.clave;
+    })) {
+      iniciarSesion(intentoLogin.usuario);
+      findUser = true;
+    }
+  }
+   if(findUser){
     return true;
   } else {
     document.getElementsByClassName("error")[0].classList.remove("ocultar");
